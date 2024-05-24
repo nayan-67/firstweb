4d3246f7cee46104986718c7e7e7b8bc55929736
nayan-67 <nayansau09@gmail.com>
2024-05-24 09:19:25 +0530

    Add files via upload

---
+++ Login_page/script.js
@@ -0,0 +1,61 @@
+let log_invisible = document.querySelector("#logv-off");
+let log_visible = document.querySelector("#logv-on");
+let reg_invisible = document.querySelector("#regv-off");
+let reg_visible = document.querySelector("#regv-on");
+let log_input = document.querySelector("#log-pass");
+let reg_input = document.querySelector("#reg-pass");
+
+function visibility_off() {
+    log_invisible.style.display = 'none';
+    log_visible.style.display = 'block';
+    reg_invisible.style.display = 'none';
+    reg_visible.style.display = 'block';
+    log_input.type = "text";
+    reg_input.type = "text";
+}
+function visibility() {
+    log_invisible.style.display = 'block';
+    log_visible.style.display = 'none';
+    reg_invisible.style.display = 'block';
+    reg_visible.style.display = 'none';
+    log_input.type = "password";
+    reg_input.type = "password";
+}
+let log_bg=document.querySelector(".login-background");
+let log_nav=document.querySelector(".login-nav");
+let log_container=document.querySelector(".login-container");
+let reg_bg=document.querySelector(".signup-background");
+let reg_nav=document.querySelector(".signup-nav");
+let reg_container=document.querySelector(".signup-container");
+
+function signup() {
+    log_bg.style.display = 'none';
+    log_nav.style.display = 'none';
+    log_container.style.display = 'none';
+    reg_bg.style.display = 'block';
+    reg_nav.style.display = 'flex';
+    reg_container.style.display = 'block';
+}
+function login() {
+    log_bg.style.display = 'block';
+    log_nav.style.display = 'flex';
+    log_container.style.display = 'block';
+    reg_bg.style.display = 'none';
+    reg_nav.style.display = 'none';
+    reg_container.style.display = 'none';
+}
+
+function submit() {
+    let reg_email=document.querySelector("#reg-email");
+    let reg_pass=document.querySelector("#reg-pass");
+    let confirm_pass=document.querySelector("#confirm-pass");
+    if (reg_email.value==='') {
+        alert("Plase fill out this")
+        return false;
+    }
+    else if (reg_pass.value!==confirm_pass.value) {
+        // confirm_pass.title="password should be same";
+        alert("Not same Password");
+        return false;
+    };
+}
