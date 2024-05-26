6a1b10ab0539800af537489bc66660ed29dbb0ab
nayan-67 <nayansau09@gmail.com>
2024-05-26 23:04:27 +0530

    Update script.js

--- Login_page/script.js
+++ Login_page/script.js
@@ -22,10 +22,10 @@ function visibility() {
     reg_input.type = "password";
 }
 let log_bg=document.querySelector(".login-background");
-let log_nav=document.querySelector(".login-nav");
+let log_nav=document.querySelector(".login_nav");
 let log_container=document.querySelector(".login-container");
 let reg_bg=document.querySelector(".signup-background");
-let reg_nav=document.querySelector(".signup-nav");
+let reg_nav=document.querySelector(".signup_nav");
 let reg_container=document.querySelector(".signup-container");
 
 function signup() {
@@ -45,18 +45,27 @@ function login() {
     reg_container.style.display = 'none';
 }
 
-function submit() {
+function reg_submit() {
     let reg_email=document.querySelector("#reg-email");
     let reg_pass=document.querySelector("#reg-pass");
     let confirm_pass=document.querySelector("#confirm-pass");
+    const regex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
     if (reg_email.value==='') {
-        alert("Plase fill out this")
+        alert("Please Enter Email")
+        reg_email.focus();
         return false;
-    }
-    else if (reg_pass.value!==confirm_pass.value) {
+    }else if(reg_email.value!==regex){
+        alert("Please Enter a Valid Email");
+        reg_email.focus();
+        return false;
+    }else if (reg_pass.value!==confirm_pass.value) {
         // confirm_pass.title="password should be same";
         alert("Not same Password");
+        confirm_pass.focus();
         return false;
+    }else{
+        alert("Registaton Successfull");
+        return true;
     };
 }
 let log_sidebar=document.querySelector('.login-sidebar');
@@ -67,19 +76,23 @@ let login_close=document.querySelector('.login-close');
 let reg_close=document.querySelector('.reg-close');
 
 log_menu.addEventListener("click",()=>{
-    log_sidebar.style.display='flex';
+    log_sidebar.style.right='0';
     log_menu.style.display='none';
+    login_close.style.display='block';
 });
 login_close.addEventListener("click",()=>{
-    log_sidebar.style.display='none';
+    log_sidebar.style.right='-200px';
     log_menu.style.display='block';
+    login_close.style.display='none';
 });
 reg_menu.addEventListener("click",()=>{
-    reg_sidebar.style.display='flex';
+    reg_sidebar.style.right='0';
     reg_menu.style.display='none';
+    reg_close.style.display='block';
 });
 reg_close.addEventListener("click",()=>{
-    reg_sidebar.style.display='none';
+    reg_sidebar.style.right='-200px';
     reg_menu.style.display='block';
+    reg_close.style.display='none';
 });
 
